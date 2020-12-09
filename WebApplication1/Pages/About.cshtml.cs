859d4031951558f5a8ff9c4d82898659ba56ca33
zhouxia <505859658@qq.com>
2020-12-09 14:28:20 +0800

    12-9-3

--- WebApplication1/Pages/About.cshtml.cs
+++ WebApplication1/Pages/About.cshtml.cs
@@ -12,7 +12,7 @@ namespace WebApplication1.Pages
 
         public void OnGet()
         {
-            Message = " 2zxtest Your application description page.";
+            Message = " 3zxtest Your application description page.";
         }
     }
 }
